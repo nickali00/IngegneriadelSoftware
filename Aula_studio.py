5be3e474b3ef4880884690ea49d8d84fab7fb9a8
Nicola Alì <nico.ali00@email.com>
2025-11-10 19:04:43 +0100

    aggiunta classe aulastudio

---
+++ Aula_studio.py
@@ -0,0 +1,10 @@
+class Aula_studio:
+    def __init__(self, nome, id, edificio, numero_tavoli, numero_postazioni):
+        self.nome = nome
+        self.id = id
+        self.edificio = edificio
+        self.numero_tavoli = numero_tavoli
+        self.numero_postazioni = numero_postazioni
+
+    def __str__(self):
+        return f"{self.nome} {self.edificio}, id: {self.id}, numero tavoli: {self.numero_tavoli}, numero postazioni: {self.numero_postazioni}"
