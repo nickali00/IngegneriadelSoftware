0c49dcb8bd0fbd3d03fcce6dddde9d9517275aeb
Nicola Alì <nico.ali00@email.com>
2025-11-10 19:17:21 +0100

    test main

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+from Amministratore import Amministratore
+from Studente import Studente
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main():
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    utenti = []
+
+    studente1 = Studente(
+        nome="Giulia",
+        cognome="Verdi",
+        data_nascita="2000-05-15",
+        codice_fiscale="VRDGLL00E55Z404A",
+        email="giulia.verdi@email.com",
+        matricola="123456",
+        facolta="Ingegneria",
+        password="password123"
+    )
+
+
+    admin1 = Amministratore(
+        nome="Luca",
+        cognome="Bianchi",
+        data_nascita="1985-11-20",
+        codice_fiscale="BNCLCU85S20E404H",
+        email="luca.bianchi@azienda.com",
+        id_amministratore="ADM1001",
+        password = "password123"
+    )
+
+    # Aggiungiamo gli utenti alla lista
+    utenti.append(studente1)
+    utenti.append(admin1)
+
+    # Mostriamo i dati di tutti gli utenti
+    print("Elenco utenti:")
+    for utente in utenti:
+        print(utente)
+
+if __name__ == "__main__":
+    main()
