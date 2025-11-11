dcf041e57f30c602ec9a81d38e08f655d1d7f620
Nicola Alì <nico.ali00@email.com>
2025-11-11 11:33:42 +0100

    riprendere la gestione di mandare utenti all'amministratore così da elencarli e avere una visione completa degli utenti registrati

--- HomeStudente.py
+++ HomeStudente.py
@@ -7,26 +7,32 @@ from profilo import Profilo
 class HomeStudente:
     def __init__(self, utente):
         self.utente = utente
+      #  self.utenti = utenti  # Lista di utenti (studenti e amministratori)
+
+        # Crea la finestra principale
         self.home_window = tk.Tk()
-        self.home_window.title(f"Home Studente: {self.utente.nome}")
+        self.home_window.title(f"Home Amministratore: {self.utente.nome}")
         self.home_window.geometry("400x400")
 
+        # Etichetta di benvenuto
         label_benvenuto = tk.Label(self.home_window, text=f"Benvenuto, {self.utente.nome}!", font=("Arial", 16))
         label_benvenuto.pack(pady=20)
 
-        # Bottone per visualizzare il profilo
-        button_profilo = tk.Button(self.home_window, text="Visualizza Profilo", command=self.mostra_profilo)
-        button_profilo.pack(pady=10)
+        # Bottone per gestire gli utenti
+        button_gestione_utenti = tk.Button(self.home_window, text="Gestisci Utenti", command=self.gestione_utenti)
+        button_gestione_utenti.pack(pady=10)
 
-        # Bottone per visualizzare gli esami
-        button_esami = tk.Button(self.home_window, text="Visualizza Esami", command=self.visualizza_esami)
-        button_esami.pack(pady=10)
+        # Bottone per visualizzare report
+        button_report = tk.Button(self.home_window, text="Visualizza Report", command=self.visualizza_report)
+        button_report.pack(pady=10)
 
         # Bottone per fare logout
         button_logout = tk.Button(self.home_window, text="Logout", command=self.logout)
         button_logout.pack(pady=20)
 
         self.home_window.mainloop()
+
+
 
     def mostra_profilo(self):
         # Crea un'istanza di Profilo e chiama il metodo per visualizzare il profilo
