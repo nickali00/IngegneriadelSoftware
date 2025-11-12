dfc389a74931fc4c378cf4bf211ff02d1ca1a14a
Nicola Tablet <nico.ali00@gmail.com>
2025-11-12 12:37:27 +0000

    push tablet aggiunte delle classi nel main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #
 from Amministratore import Amministratore
 from Studente import Studente
-from login import login
+from Aula_studio import Aulastudio 
+from tavolo import Tavolo
+#from login import login
 
 
 def main():
@@ -45,12 +47,21 @@ def main():
     utenti.append(studente2)
     utenti.append(admin1)
 
+    # Creazione di 2 aule
+    aula1 = Aulastudio("Sala A", 101, "Edificio Centrale", 10, 50)
+    aula2 = Aulastudio("Sala B", 102, "Edificio Sud", 8, 40)
+
+    # Inserimento in un vettore (lista)
+    aule = [aula1, aula2]
+    # Creazione di 2 tavoli per ciascuna aula
+    tavolo1_aula1 = Tavolo(1, 10)
+    tavolo2_aula1 = Tavolo(2, 10)
     # Mostriamo i dati di tutti gli utenti
     print("Elenco utenti:")
     for utente in utenti:
         print(utente)
 
-    login(utenti)
+ #   login(utenti)
 
 if __name__ == "__main__":
     main()
