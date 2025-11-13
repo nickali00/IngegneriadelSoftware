81559b10d7cc5b51df60129f668d0c70f4989285
Nicola Alì <nico.ali00@email.com>
2025-11-13 18:23:20 +0100

    main.py riopulito da utenti creati staticamente

--- main.py
+++ main.py
@@ -7,46 +7,6 @@ from login import login
 
 
 def main():
-
-    utenti = []
-
-    studente1 = Studente(
-        nome="Giulia",
-        cognome="Verdi",
-        data_nascita="2000-05-15",
-        codice_fiscale="VRDGLL00E55Z404A",
-        email="giulia.verdi@email.com",
-        matricola="123456",
-        facolta="Ingegneria",
-        password="password123"
-    )
-
-    studente2 = Studente(
-        nome="Marco",
-        cognome="Rossi",
-        data_nascita="2001-09-10",
-        codice_fiscale="RSSMRC01P10H501K",
-        email="marco.rossi@email.com",
-        matricola="654321",
-        facolta="Informatica",
-        password="securepass456"
-    )
-
-    admin1 = Amministratore(
-        nome="Luca",
-        cognome="Bianchi",
-        data_nascita="1985-11-20",
-        codice_fiscale="BNCLCU85S20E404H",
-        email="luca.bianchi@azienda.com",
-        id_amministratore="ADM1001",
-        password = "password123"
-    )
-
-    # Aggiungiamo gli utenti alla lista
-    utenti.append(studente1)
-    utenti.append(studente2)
-    utenti.append(admin1)
-
     # Creazione di 2 aule
     aula1 = Aulastudio("Sala A", 101, "Edificio Centrale", 10, 50)
     aula2 = Aulastudio("Sala B", 102, "Edificio Sud", 8, 40)
@@ -57,9 +17,6 @@ def main():
     tavolo1_aula1 = Tavolo(1, 10)
     tavolo2_aula1 = Tavolo(2, 10)
     # Mostriamo i dati di tutti gli utenti
-    print("Elenco utenti:")
-    for utente in utenti:
-        print(utente)
     
     login()
 
