0c392dff8380c7ae9cdcb7f7a6ad794663099554
Nicola Tablet <nico.ali00@gmail.com>
2025-11-14 10:23:20 +0000

    aggiunta la possibilità da parte dell'amministratore di aggiungere una nuova aula e tavolo

---
+++ nuova_aula.py
@@ -0,0 +1,104 @@
+import tkinter as tk
+from tkinter import messagebox, ttk
+from mysql.connector import Error
+from connessione import connect_to_db
+
+def nuovaaula():
+    reg_window = tk.Tk()
+    reg_window.title("Registrazione")
+    reg_window.geometry("300x550")
+
+    label_nome = tk.Label(reg_window, text="Nome:")
+    label_nome.pack(pady=5)
+    entry_nome = tk.Entry(reg_window)
+    entry_nome.pack(pady=5)
+
+    label_corso = tk.Label(reg_window, text="Seleziona edificio:")
+    label_corso.pack(pady=5)
+
+    edifici = {}
+    conn = connect_to_db()
+    if conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, nome FROM edificio")
+        for row in cursor.fetchall():
+            edifici[row[1]] = row[0]  # chiave = nome corso, valore = id
+        cursor.close()
+        conn.close()
+    else:
+        messagebox.showerror("Errore", "Connessione al database fallita!")
+        reg_window.destroy()
+        return
+    edificio_var = tk.StringVar()
+    combo_edifici = ttk.Combobox(
+        reg_window,
+        textvariable=edificio_var,
+        values=list(edifici.keys()),
+        state="readonly"  # obbligatorio per evitare input manuale
+    )
+    combo_edifici.pack(pady=5)
+    combo_edifici.current(0)  # seleziona il primo elemento di default
+
+    # Callback per aggiornare il valore selezionato
+    def on_corso_selected(event):
+        # aggiorna corso_var con il valore selezionato
+        edificio_var.set(combo_edifici.get())
+
+    combo_corsi.bind("<<ComboboxSelected>>", on_corso_selected)
+
+    def aggiungiaula():
+        nome = entry_nome.get()
+        edificio_nome = edificio_var.get()
+        ed = edifici.get(edificio_nome)
+
+        if not nome:
+            messagebox.showerror("Errore", "Inserisci il nome!")
+            return
+        if not ed:
+            messagebox.showerror("Errore", "Seleziona un edificio!")
+            return
+
+
+            # Inserimento nel database
+        conn = connect_to_db()
+
+        try:
+            cursor = conn.cursor()
+            query = """
+                       INSERT INTO aula (nome, fkedificio)
+                       VALUES (%s, %s)
+                   """
+            cursor.execute(query, (nome, ed))
+            conn.commit()
+            messagebox.showinfo("Registrazione", "Edificio aggiunto con successo!")
+        except Error as e:
+            messagebox.showerror("Errore database", str(e))
+        finally:
+            cursor.close()
+            conn.close()
+
+
+        from login import login
+        reg_window.destroy()
+        
+
+
+
+    frame_bottoni = tk.Frame(reg_window)
+    frame_bottoni.pack(pady=20)
+
+    def annulla():
+        try:
+            reg_window.destroy()
+        except tk.TclError:
+            pass
+
+
+    button_annulla = tk.Button(frame_bottoni, text="Annulla", command=annulla)
+    button_annulla.pack(side=tk.LEFT, padx=10)
+
+    button_registra = tk.Button(frame_bottoni, text="Aggiungi", command=aggiungiaula)
+    button_registra.pack(side=tk.LEFT, padx=10)
+
+    reg_window.mainloop()
+
---
+++ nuovo_tavolo.py
@@ -0,0 +1,101 @@
+import tkinter as tk
+from tkinter import messagebox, ttk
+from mysql.connector import Error
+from connessione import connect_to_db
+
+def nuovotavolo():
+    reg_window = tk.Tk()
+    reg_window.title("Registrazione")
+    reg_window.geometry("300x550")
+
+    label_posti = tk.Label(reg_window, text="Numero posti:")
+    label_posti.pack(pady=5)
+    entry_posti = tk.Entry(reg_window)
+    entry_posti.pack(pady=5)
+
+    label_aula = tk.Label(reg_window, text="Seleziona aula:")
+    label_aula.pack(pady=5)
+
+    aule = {}
+    conn = connect_to_db()
+    if conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, nome FROM aula")
+        for row in cursor.fetchall():
+            aule[row[1]] = row[0]  # chiave = nome corso, valore = id
+        cursor.close()
+        conn.close()
+    else:
+        messagebox.showerror("Errore", "Connessione al database fallita!")
+        reg_window.destroy()
+        return
+    aula_var = tk.StringVar()
+    combo_aule = ttk.Combobox(
+        reg_window,
+        textvariable=aula_var,
+        values=list(corsi.keys()),
+        state="readonly"  # obbligatorio per evitare input manuale
+    )
+    combo_aule.pack(pady=5)
+    combo_aule.current(0)  # seleziona il primo elemento di default
+
+    # Callback per aggiornare il valore selezionato
+    def on_corso_selected(event):
+        # aggiorna corso_var con il valore selezionato
+        aula_var.set(combo_aule.get())
+
+    combo_aule.bind("<<ComboboxSelected>>", on_corso_selected)
+
+    def nuovo_tavolo():
+        numero = entry_posti.get()
+        aula_nome = aula_var.get()
+        au = aule.get(aula_nome)
+
+        if not numero:
+            messagebox.showerror("Errore", "Inserisci il numero!")
+            return
+        if not au:
+            messagebox.showerror("Errore", "Seleziona un aula!")
+            return
+
+            # Inserimento nel database
+        conn = connect_to_db()
+
+        try:
+            cursor = conn.cursor()
+            query = """
+                       INSERT INTO tavolo (numeroposti, fkaula)
+                       VALUES (%s, %s)
+                   """
+            cursor.execute(query, (numero, au))
+            conn.commit()
+            messagebox.showinfo("Registrazione", "aula aggiunta con successo!")
+        except Error as e:
+            messagebox.showerror("Errore database", str(e))
+        finally:
+            cursor.close()
+            conn.close()
+
+
+        reg_window.destroy()
+
+
+
+
+    frame_bottoni = tk.Frame(reg_window)
+    frame_bottoni.pack(pady=20)
+
+    def annulla():
+        try:
+            reg_window.destroy()
+        except tk.TclError:
+            pass
+
+    button_annulla = tk.Button(frame_bottoni, text="Annulla", command=annulla)
+    button_annulla.pack(side=tk.LEFT, padx=10)
+
+    button_registra = tk.Button(frame_bottoni, text="aggiungi", command=nuovo_tavolo)
+    button_registra.pack(side=tk.LEFT, padx=10)
+
+    reg_window.mainloop()
+
